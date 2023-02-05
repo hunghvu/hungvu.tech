ef9634ed5507b1745513d8838c7a80dcc585f352
hunghvu <hunghvu2017@gmail.com>
2023-02-04 19:19:47 -0800

    feat: add local fonts

---
+++ apps/web/app/fonts.ts
@@ -0,0 +1,26 @@
+/**
+ * @author Hung Vu 
+ * 
+ * This file provides local fonts to be used across the application
+ */
+import localfont from "@next/font/local"
+
+export const fredoka = localfont({
+  src: "../public/fonts/Fredoka-Variable.ttf"
+})
+
+export const interTight = localfont({
+  src: "../public/fonts/InterTight-Variable.ttf"
+})
+
+export const interTightItalic = localfont({
+  src: "../public/fonts/InterTight-Italic-Variable.ttf"
+})
+
+export const robotoSerif = localfont({
+  src: "../public/fonts/RobotoSerif-Variable.ttf"
+})
+
+export const robotoSerifItalic = localfont({
+  src: "../public/fonts/RobotoSerif-Italic-Variable.ttf"
+})
