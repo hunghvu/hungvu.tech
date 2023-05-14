35a601cca3b42b199f86991e08080bd7cd0bb8de
hunghvu <hunghvu2017@gmail.com>
2023-05-14 09:42:25 -0700

    chore: add comments about rules when applying fonts

--- apps/web/app/fonts.ts
+++ apps/web/app/fonts.ts
@@ -2,13 +2,23 @@
  * @author Hung Vu 
  * 
  * This file provides local fonts to be used across the application
+ * 
+ * General rules:
+ * Section title: 32 px or equivalent
+ * Section subtitle: 28 px or equivalent
+ * H2, more emphasis: 24 px or equivalent
+ * H3, less emphasis: 20 px or equivalent
+ * Paragraph: 16 px or equivalent
+ * Minor additonal (meta) details: 12 px or equivalent
  */
 import localfont from "next/font/local"
 
+// Generally used for buttons
 export const fredoka = localfont({
   src: "../public/fonts/Fredoka-Variable.ttf"
 })
 
+// Generally used for paragraphs
 export const interTight = localfont({
   src: "../public/fonts/InterTight-Variable.ttf"
 })
@@ -17,6 +27,7 @@ export const interTightItalic = localfont({
   src: "../public/fonts/InterTight-Italic-Variable.ttf"
 })
 
+// Generally used for titles
 export const robotoSerif = localfont({
   src: "../public/fonts/RobotoSerif-Variable.ttf"
 })
