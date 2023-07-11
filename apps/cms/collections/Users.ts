e5cbc6479279f304aba68fb743b4bafddd1c3a64
hunghvu <hunghvu2017@gmail.com>
2023-07-10 21:27:55 -0700

    fix: broken Payload login flow in local environment

--- apps/cms/collections/Users.ts
+++ apps/cms/collections/Users.ts
@@ -2,19 +2,22 @@ import { CollectionConfig } from 'payload/types';
 
 const Users: CollectionConfig = {
   slug: 'users',
-  auth: {
-    // TODO: enable 2FA, perhaps via Passport
-    // TODO: enable email verification
-    // TODO: enable password recovery (optional?)
-    cookies: {
-      secure: true,
-      sameSite: 'none',
-      domain: process.env.FRONT_END_DOMAIN! ?? 'http://localhost:3000',
-    },
-    tokenExpiration: 21600,
-    maxLoginAttempts: 5,
-    lockTime: 300,
-  },
+  // Secure cookies breaks login flow in localhost environment
+  auth: process.env.FRONT_END_DOMAIN
+    ? {
+        // TODO: enable 2FA, perhaps via Passport
+        // TODO: enable email verification
+        // TODO: enable password recovery (optional?)
+        cookies: {
+          secure: true,
+          sameSite: 'none',
+          domain: process.env.FRONT_END_DOMAIN,
+        },
+        tokenExpiration: 21600,
+        maxLoginAttempts: 5,
+        lockTime: 300,
+      }
+    : true,
   admin: {
     useAsTitle: 'email',
   },
