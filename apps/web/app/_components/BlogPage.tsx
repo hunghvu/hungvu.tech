e36cd73876d65c4e0d31d8f16501861e8715c7ef
hunghvu <hunghvu2017@gmail.com>
2023-11-10 19:37:23 -0800

    feat: make home page responsive, and remove heavy elements to reduce load time

--- apps/web/app/_components/BlogPage.tsx
+++ apps/web/app/_components/BlogPage.tsx
@@ -4,60 +4,53 @@
  * Blog page is a page that lists all the articles in a timeline fashion.
  */
 
-"use client"
-import Link from "next/link";
-import { Button } from "primereact/button";
-import { Card } from "primereact/card";
-import { ScrollPanel } from "primereact/scrollpanel";
-import { Tag } from "primereact/tag";
-import { Timeline } from "primereact/timeline";
-import { geist } from "app/_components/fonts";
+'use client'
+import Link from 'next/link';
+import { Card } from 'primereact/card';
+import { Tag } from 'primereact/tag';
+import { Timeline } from 'primereact/timeline';
+import { geist } from 'app/_components/fonts';
 
 const BlogPage: React.FunctionComponent = () => {
   const data = [
     {
-      title: "Homelab", description: `This is my homelab This is my homelab This is my homelab This is my homelab This is my homelab This is my homelab This is my homelab This is my homelab
-    This is my homelab This is my homelab This is my homelab This is my homelab This is my homelab This is my homelab This is my homelab This is my homelab`, date: "2021-10-15", url: "/homelab", tags: ["homelab", "personal", "hobbies"]
+      title: 'Homelab', description: `This is my homelab This is my homelab This is my homelab This is my homelab This is my homelab This is my homelab This is my homelab This is my homelab
+    This is my homelab This is my homelab This is my homelab This is my homelab This is my homelab This is my homelab This is my homelab This is my homelab`, date: '2021-10-15', url: '/homelab', tags: ['homelab', 'personal', 'hobbies']
     },
-    { title: "Blog", description: "This is my blog", date: "2021-10-15", url: "/blog", tags: ["blogging", "writing", "personal"] },
-    { title: "About", description: "This is my about page", date: "2021-10-15", url: "/about", tags: ["about", "personal", "bio"] },
-    { title: "Contact", description: "This is my contact page", date: "2021-10-15", url: "/contact", tags: ["contact", "personal", "email"] },
-    { title: "Projects", description: "This is my projects page", date: "2021-10-15", url: "/projects", tags: ["projects", "coding", "portfolio"] },
-    { title: "Resume", description: "This is my resume page", date: "2021-10-15", url: "/resume", tags: ["resume", "career", "job"] },
-    { title: "Timeline", description: "This is my timeline page", date: "2021-10-15", url: "/timeline", tags: ["timeline", "personal", "history"] },
-    { title: "404", description: "This is my 404 page", date: "2021-10-15", url: "/404", tags: ["404", "error", "web"] },
-    { title: "500", description: "This is my 500 page", date: "2021-10-15", url: "/500", tags: ["500", "error", "web"] },
-    { title: "Login", description: "This is my login page", date: "2021-10-15", url: "/login", tags: ["login", "security", "web"] },
-    { title: "Register", description: "This is my register page", date: "2021-10-15", url: "/register", tags: ["register", "security", "web"] },
-    { title: "Forgot Password", description: "This is my forgot password page", date: "2021-10-15", url: "/forgot-password", tags: ["forgot password", "security", "web"] },
-    { title: "Reset Password", description: "This is my reset password page", date: "2021-10-15", url: "/reset-password", tags: ["reset password", "security", "web"] },
-    { title: "Verify Email", description: "This is my verify email page", date: "2021-10-15", url: "/verify-email", tags: ["verify email", "security", "web"] },
-    { title: "Verify Email Sent", description: "This is my verify email sent page", date: "2021-10-15", url: "/verify-email-sent", tags: ["verify email", "security", "web"] },
-    { title: "Verify Email Error", description: "This is my verify email error page", date: "2021-10-15", url: "/verify", tags: ["verify email", "security", "web"] },
+    { title: 'Blog', description: 'This is my blog', date: '2021-10-15', url: '/blog', tags: ['blogging', 'writing', 'personal'] },
+    { title: 'About', description: 'This is my about page', date: '2021-10-15', url: '/about', tags: ['about', 'personal', 'bio'] },
+    { title: 'Contact', description: 'This is my contact page', date: '2021-10-15', url: '/contact', tags: ['contact', 'personal', 'email'] },
+    { title: 'Projects', description: 'This is my projects page', date: '2021-10-15', url: '/projects', tags: ['projects', 'coding', 'portfolio'] },
+    { title: 'Resume', description: 'This is my resume page', date: '2021-10-15', url: '/resume', tags: ['resume', 'career', 'job'] },
+    { title: 'Timeline', description: 'This is my timeline page', date: '2021-10-15', url: '/timeline', tags: ['timeline', 'personal', 'history'] },
+    { title: '404', description: 'This is my 404 page', date: '2021-10-15', url: '/404', tags: ['404', 'error', 'web'] },
+    { title: '500', description: 'This is my 500 page', date: '2021-10-15', url: '/500', tags: ['500', 'error', 'web'] },
+    { title: 'Login', description: 'This is my login page', date: '2021-10-15', url: '/login', tags: ['login', 'security', 'web'] },
+    { title: 'Register', description: 'This is my register page', date: '2021-10-15', url: '/register', tags: ['register', 'security', 'web'] },
+    { title: 'Forgot Password', description: 'This is my forgot password page', date: '2021-10-15', url: '/forgot-password', tags: ['forgot password', 'security', 'web'] },
+    { title: 'Reset Password', description: 'This is my reset password page', date: '2021-10-15', url: '/reset-password', tags: ['reset password', 'security', 'web'] },
+    { title: 'Verify Email', description: 'This is my verify email page', date: '2021-10-15', url: '/verify-email', tags: ['verify email', 'security', 'web'] },
+    { title: 'Verify Email Sent', description: 'This is my verify email sent page', date: '2021-10-15', url: '/verify-email-sent', tags: ['verify email', 'security', 'web'] },
+    { title: 'Verify Email Error', description: 'This is my verify email error page', date: '2021-10-15', url: '/verify', tags: ['verify email', 'security', 'web'] },
   ];
 
   const customizedContent = (item: any) => {
     return (
       <Link href={item.url}>
         <Card title={<h2>{item.title}</h2>}
-          role="article"
+          role='article'
           footer={
-            <Button
-              link
-              size="small"
-              aria-label={`View blog article ${item.title} now.`}
-              pt={{
-                root: { className: `p-0 ${geist.className}` }
-              }}
-            >View blog article</Button>}
+            <div className="lg:hidden">
+              {customizedOpposite(item)}
+            </div>
+          }
           pt={{
             root: { className: `-mt-4 mb-12 pb-6 bg-transparent hover:bg-dark-cyan-800/20 hover:rounded-2xl ${geist.className}` },
-            body: { className: "px-4 py-4" },
-            title: { className: "text-lg md:text-xl lg:text-2xl" },
-            content: { className: "text-sm md:text-base lg:text-lg" },
-            footer: { className: "pt-0" }
+            body: { className: 'px-4 py-4' },
+            title: { className: 'text-lg md:text-xl lg:text-2xl' },
+            content: { className: 'text-sm md:text-base lg:text-lg' },
           }}>
-          <p>{item.description}</p>
+          <p className="text-[#ffffffde]/80 line-clamp-2">{item.description}</p>
         </Card>
       </Link>
 
@@ -66,44 +59,46 @@ const BlogPage: React.FunctionComponent = () => {
 
   const customizedOpposite = (item: any) => {
     return (
-      <aside className="flex flex-col items-end gap-2">
+      <aside className='flex lg:flex-col items-end gap-2 text-[#ffffffde]/60'>
         {/* TODO: Use proper time format from the API */}
-        <time className="text-xs md:text-sm lg:text-base w-[20vw]" dateTime={item.date}>{item.date}</time>
+        <div className='flex flex-row lg:flex-row-reverse gap-1 mr-8 lg:mr-0 items-center justify-center'>
+          <span className='pi pi-calendar'></span>
+          <time className='text-xs md:text-sm lg:text-base min-w-[85px]' dateTime={item.date}>{item.date}</time>
+        </div>
+
 
         {/* TODO: Use db id as key instead */}
         {item.tags.map((tag: string) => <Tag key={tag} value={tag}
-          severity="info" role="tag" aria-label={`The article ${item.title} belongs to the topic ${tag}.`}
+          severity='info' aria-label={`The article ${item.title} belongs to the topic ${tag}.`}
           pt={{
-            root: { className: "max-w-[20vw]" },
-            value: { className: `text-xs md:text-sm lg:text-base ${geist.className}` }
+            root: { className: 'py-0 bg-dark-cyan-800' },
+            value: { className: `text-xs md:text-sm lg:text-base ${geist.className} text-[#ffffffde]/60` }
           }}></Tag>)}
       </aside>
     )
   }
   return (
-    <section className="flex flex-row">
-      <ScrollPanel pt={{
-        root: { className: "max-w-[1024px] lg:w-[1024px] h-[80vh]" }
-      }}>
-        <Timeline value={data} opposite={customizedOpposite} content={customizedContent} pt={{
-          root: {
-            className: geist.className
-          },
-          marker: {
-            className: "bg-[#bdbdbd]/40"
-          },
-          connector: {
-            className: "bg-[#bdbdbd]/20"
-          },
-          opposite: {
-            className: "flex grow-0 text-sm md:text-base lg:text-lg",
-          },
-          content: {
-            className: "flex grow",
-          }
-        }} />
-      </ScrollPanel>
-
+    <section className='flex flex-row justify-center items-center w-full'>
+      <Timeline value={data} opposite={customizedOpposite} content={customizedContent} pt={{
+        root: {
+          className: `${geist.className} flex flex-col justify-center items-center`
+        },
+        event: {
+          className: 'w-full lg:w-[1024px]'
+        },
+        marker: {
+          className: 'bg-[#bdbdbd]/40'
+        },
+        separator: {
+          className: 'hidden lg:flex'
+        },
+        connector: {
+          className: 'bg-[#bdbdbd]/20'
+        },
+        opposite: {
+          className: 'hidden lg:flex lg:flex-grow-0'
+        }
+      }} />
     </section>
   )
 }
