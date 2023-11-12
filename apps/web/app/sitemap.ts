073fe6f1c22645ab7c7ff14490a67b25e6f371a3
hunghvu <hunghvu2017@gmail.com>
2023-11-12 13:49:29 -0800

    feat: add sitemap

---
+++ apps/web/app/sitemap.ts
@@ -0,0 +1,34 @@
+import { MetadataRoute } from 'next'
+
+
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const sites: MetadataRoute.Sitemap = [
+    {
+      url: process.env.NEXT_PUBLIC_BASE_URL!,
+      lastModified: new Date(),
+      changeFrequency: 'always',
+      priority: 1,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL!}/homelab`,
+      lastModified: new Date(),
+      changeFrequency: 'always',
+      priority: 1,
+    },
+  ]
+
+  const articles = await fetch(process.env.PAYLOAD_SERVER_ARTICLES_URL!, {
+    cache: process.env.NODE_ENV === 'production' ? 'force-cache' : 'no-store',
+  });
+  (await articles.json()).docs.map((article: any) => {
+    sites.push({
+      url: `${process.env.NEXT_PUBLIC_BASE_URL!}/article/${article.settings.urlSlug}`,
+      lastModified: new Date(article.updatedAt),
+      changeFrequency: 'daily',
+      priority: 0.8,
+    })
+  });
+
+  return sites;
+}
