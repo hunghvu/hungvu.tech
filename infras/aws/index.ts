a088df56825f38436edc2bf82d6c5547b78570de
hunghvu <hunghvu2017@gmail.com>
2023-12-03 15:35:45 -0800

    style: minor rename to improve consistency

--- infras/aws/index.ts
+++ infras/aws/index.ts
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // eslint-disable-next-line import/first
-import * as dev from "./services";
+import * as aws from "./services";
 
-export { dev };
+export { aws };
--- infras/aws/services/index.ts
+++ infras/aws/services/index.ts
@@ -4,11 +4,11 @@
  * Module entry point
  */
 import * as vpc from "./vpc"
-import * as compute from "./ec2"
+import * as ec2 from "./ec2"
 
 
 // Duplicate export because service such as a VPC
 // is used by other services such as EC2. However,
 // Pulumi can prevent circular deployment with its 
 // its own mechanism.
-export { vpc, compute }
+export { vpc, ec2 }
