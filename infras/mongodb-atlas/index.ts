205f59cb9591f2e47e666e56390d9071becc176e
hunghvu <hunghvu2017@gmail.com>
2023-12-04 21:35:05 -0800

    chore: remove unused dependencies

--- infras/mongodb-atlas/index.ts
+++ infras/mongodb-atlas/index.ts
@@ -1,8 +1,3 @@
-// eslint-disable-next-line import/newline-after-import, import/order
-import dotenv from "dotenv";
-dotenv.config();
-
-// eslint-disable-next-line import/first
 import * as mongodbAtlas from "./services";
 
 export { mongodbAtlas };
