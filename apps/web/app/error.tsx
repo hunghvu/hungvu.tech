2f6975fc11c2f37976008cbe984ad4e59f100509
hunghvu <hunghvu2017@gmail.com>
2023-12-06 20:40:15 -0800

    feat: hide error message (many times they are irrelevant for end users)

--- apps/web/app/error.tsx
+++ apps/web/app/error.tsx
@@ -24,8 +24,8 @@ const Error = ({ error }: { error: Error & { digest?: string }; reset: () => voi
   return (
     <section className='flex flex-col justify-center items-center gap-4 w-full md:w-[768px] m-4'>
       <hgroup className='flex flex-col justify-center items-center gap-6 w-full'>
-        <h1 className='text-5xl md:text-6xl font-extrabold'>{error.message} 🤔</h1>
-        <p className='text-2xl md:text-3xl font-bold text-[#ffffffde]/70 italic text-center'>Something went wrong. Digest: {error.digest}.</p>
+        <h1 className='text-5xl md:text-6xl font-extrabold'>Something is off. 🤔</h1>
+        <p className='text-2xl md:text-3xl font-bold text-[#ffffffde]/70 italic text-center'>Digest: {error.digest}.</p>
         <div className='flex flex-row gap-4 justify-center items-center'>
           <Link className='text-lg md:text-xl hover:font-semibold bg-dark-cyan-700 py-1 w-[10rem] md:w-[12rem] rounded-md text-center' href='/'>
             Take me home
