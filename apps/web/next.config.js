21aef9bc21b924e35be0e872e5b09d66229acd40
hunghvu <hunghvu2017@gmail.com>
2023-12-10 07:58:14 -0800

    fix: attempt to fix #56, remotePatterns not detect in production

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,10 +3,8 @@ module.exports = {
   images: {
     remotePatterns: [
       {
-        protocol: process.env.NEXT_PUBLIC_REMOTE_IMAGE_PROTOCOL,
-        hostname: process.env.NEXT_PUBLIC_REMOTE_IMAGE_HOSTNAME,
-        port: process.env.NEXT_PUBLIC_REMOTE_IMAGE_PORT,
-        pathname: process.env.NEXT_PUBLIC_REMOTE_IMAGE_PATH,
+        protocol: "https",
+        hostname: "**.hungvu.tech",
       },
     ],
   },
