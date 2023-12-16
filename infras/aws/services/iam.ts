4ef739b57bd70bc2f93f51870017863a35d6463b
hunghvu <hunghvu2017@gmail.com>
2023-12-16 12:34:45 -0800

    feat: allow ec2 instance to have ssm access and store CloudWatch Agent config file, relates to #51

--- infras/aws/services/iam.ts
+++ infras/aws/services/iam.ts
@@ -1,3 +1,9 @@
+/**
+ * Author: Hung Vu
+ * 
+ * Configure IAM role for EC2 instance.
+ */
+
 import * as pulumi from "@pulumi/pulumi";
 import * as awsClassic from "@pulumi/aws";
 
@@ -17,9 +23,28 @@ const ec2Role = new awsClassic.iam.Role("ec2Role", {
   }),
 });
 
+// IAM Policy that allows ssm:PutParameter
+const ec2PutParameterPolicy = new awsClassic.iam.Policy("ec2PutParameterPolicy", {
+  description: "Policy that allows ssm:PutParameter",
+  policy: {
+    Version: "2012-10-17",
+    Statement: [{
+      Action: "ssm:PutParameter",
+      Resource: "*",
+      Effect: "Allow",
+    }],
+  },
+});
+
 // Attach the CloudWatchAgentServerPolicy to the role
-const ec2PolicyAttachment = new awsClassic.iam.PolicyAttachment("cloudWatchAgentServerRoleAttachment", {
+const ec2PolicyAttachment = new awsClassic.iam.PolicyAttachment("ec2CloudWatchAgentServerRoleAttachment", {
   policyArn: "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy",
+  roles: [ec2Role.name],
+});
+
+// Attach the ec2PutParameterPolicy to the role
+const ec2PutParameterPolicyAttachment = new awsClassic.iam.PolicyAttachment("ec2PutParameterPolicyAttachment", {
+  policyArn: ec2PutParameterPolicy.arn,
   roles: [ec2Role.name],
 });
 
@@ -29,5 +54,7 @@ export const ec2InstanceProfile = new awsClassic.iam.InstanceProfile("ec2Instanc
 });
 
 export const ec2RoleId = ec2Role.id;
+export const ec2PutParameterPolicyId = ec2PutParameterPolicy.id;
 export const ec2PolicyAttachmentId = ec2PolicyAttachment.id;
+export const ec2PutParameterPolicyAttachmentId = ec2PutParameterPolicyAttachment.id;
 export const ec2InstanceProfileId = ec2InstanceProfile.id;
