84b21b6e15886fefc4904ef771f83da7157de02f
hunghvu <hunghvu2017@gmail.com>
2024-02-25 11:43:54 -0800

    feat: Add JSON LD to OpenWRT ToH page, related to #137

--- apps/web/app/lab/openwrt-table-of-hardware/page.tsx
+++ apps/web/app/lab/openwrt-table-of-hardware/page.tsx
@@ -2,6 +2,7 @@ import getOpenwrtToh from '@utils/request/server-side/lab/openwrt-table-of-harwa
 import getOpenwrtTohAvailableValuesOfEachField from '@utils/request/server-side/lab/openwrt-table-of-harware/get-openwrt-toh-available-values-of-each-field';
 import getLabBySlug from '@utils/request/server-side/lab/get-lab-by-slug';
 import type { Viewport, Metadata } from 'next';
+import { ArticleJsonLd } from 'next-seo';
 import PageOpenwrtToh from './page-openwrt-toh';
 
 export const viewport: Viewport = {
@@ -11,32 +12,32 @@ export const viewport: Viewport = {
 };
 
 export const generateMetadata = async (): Promise<Metadata> => {
-  const content = await getLabBySlug('openwrt-table-of-hardware');
+  const contentData = await getLabBySlug('openwrt-table-of-hardware');
   return {
-    title: `${content.settings.seoTitle} - hungvu.tech`,
-    description: `${content.settings.seoDescription}`,
+    title: `${contentData.settings.seoTitle} - hungvu.tech`,
+    description: `${contentData.settings.seoDescription}`,
     alternates: {
-      canonical: `/lab/${content.settings.slug}`,
+      canonical: `/lab/${contentData.settings.slug}`,
     },
     openGraph: {
       siteName: 'hungvu.tech',
-      title: `${content.settings.seoTitle} - hungvu.tech`,
-      description: `${content.settings.seoDescription}`,
+      title: `${contentData.settings.seoTitle} - hungvu.tech`,
+      description: `${contentData.settings.seoDescription}`,
       type: 'article',
-      url: `/lab/${content.settings.slug}`,
+      url: `/lab/${contentData.settings.slug}`,
       images: {
-        url: `${content.settings.images.sizes.og.url}`,
-        alt: `${content.settings.seoTitle} - hungvu.tech`,
+        url: `${contentData.settings.images.sizes.og.url}`,
+        alt: `${contentData.settings.seoTitle} - hungvu.tech`,
       },
       locale: 'en_US',
     },
     twitter: {
       card: 'summary_large_image',
-      title: `${content.settings.seoTitle} - hungvu.tech`,
-      description: `${content.settings.seoDescription}`,
+      title: `${contentData.settings.seoTitle} - hungvu.tech`,
+      description: `${contentData.settings.seoDescription}`,
       images: {
-        url: `${content.settings.images.sizes.og.url}`,
-        alt: `${content.settings.seoTitle} - hungvu.tech`,
+        url: `${contentData.settings.images.sizes.og.url}`,
+        alt: `${contentData.settings.seoTitle} - hungvu.tech`,
       },
       creator: '@hunghvu_dev',
     },
@@ -47,7 +48,34 @@ const Page = async (): Promise<any> => {
   const data = await getOpenwrtToh(10, 1);
   const availableValues = await getOpenwrtTohAvailableValuesOfEachField();
   const contentData = await getLabBySlug('openwrt-table-of-hardware');
-  return <PageOpenwrtToh availableValues={availableValues} contentData={contentData} tohData={data} />;
+  return (
+    <>
+      <ArticleJsonLd
+        authorName={[
+          {
+            name: 'Hung Vu',
+            url: process.env.NEXT_PUBLIC_BASE_URL!,
+          },
+        ]}
+        dateModified={contentData.updatedAt}
+        datePublished={contentData.createdAt}
+        description={contentData.settings.seoDescription}
+        images={[
+          `${contentData.settings.images.sizes.cover.url}`,
+          `${contentData.settings.images.sizes.og.url}`,
+          `${contentData.settings.images.sizes.embed.url}`,
+          `${contentData.settings.images.sizes.thumbnail.url}`,
+        ]}
+        isAccessibleForFree
+        publisherLogo={`${process.env.NEXT_PUBLIC_BASE_URL!}/favicon.ico`}
+        publisherName='Hung Vu - hungvu.tech'
+        title={contentData.settings.seoTitle}
+        url={`${process.env.NEXT_PUBLIC_BASE_URL!}/lab/${contentData.settings.slug}`}
+        useAppDir
+      />
+      <PageOpenwrtToh availableValues={availableValues} contentData={contentData} tohData={data} />;
+    </>
+  );
 };
 
 export default Page;
