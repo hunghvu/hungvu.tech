ab47ee94da6083dac477e089470a2a2a974187c6
hunghvu <hunghvu2017@gmail.com>
2024-06-25 20:58:21 -0700

    chore: flatten and simplify project structure, and moving away from monorepo for v2.0, related to #166

--- infras/iaac/aws/index.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/newline-after-import, import/order
-import dotenv from "dotenv";
-dotenv.config();
-
-// eslint-disable-next-line import/first
-import * as aws from "./services";
-
-export { aws };
--- infras/iaac/cloudflare/services/index.ts
+++
@@ -1,3 +0,0 @@
-import * as r2 from "./r2";
-
-export { r2 }
