dec617f233ddc3d441d18106033d1769e81e740b
hunghvu <hunghvu2017@gmail.com>
2024-07-13 15:54:28 -0700

    feat: update frontmatter schemea for blog post, related to #171

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,12 +4,21 @@ const blog = defineCollection({
 	type: 'content',
 	// Type-check frontmatter using a schema
 	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
+		// Viewable on a blog post page
+		readerTitle: z.string(),
+		readerDescription: z.string(),
+		coverImage: z.string(),
+		coverAlt: z.string(),
+
+		// Viewable, but sort of metadata-like information
+		publishDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+		tags: z.array(z.string()),
+		series: z.string().optional(),
+
+		// Not viewable, but used for SEO
+		seoTitle: z.string(),
+		seoDescription: z.string(),
 	}),
 });
 
