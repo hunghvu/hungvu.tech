5bca35ac972f027f46267723c19ba39628cf6606
hunghvu <hunghvu2017@gmail.com>
2024-10-23 19:54:53 -0700

    feat: config prettier and eslint, #178

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+
+export default [
+  // add more generic rule sets here, such as:
+  // js.configs.recommended,
+  ...eslintPluginAstro.configs["jsx-a11y-strict"],
+  {
+    rules: {
+      // override/add rules settings here, such as:
+      // "astro/no-set-html-directive": "error"
+    }
+  }
+];
