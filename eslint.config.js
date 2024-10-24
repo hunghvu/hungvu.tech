b390968c6318383120d0a21f829d73cae834bd6d
hunghvu <hunghvu2017@gmail.com>
2024-10-23 21:12:09 -0700

    chore: lint codebase, #178

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,20 @@
-import eslintPluginAstro from "eslint-plugin-astro";
+import pluginAstro from "eslint-plugin-astro";
+// eslint-plugin-tailwindcss does not support astro yet
+// typescript-eslint is not intended for astro files
+
+// eslint-config-prettier is for turning rules that conflict with prettier
+import pluginPrettier from "eslint-config-prettier";
 
 export default [
-  // add more generic rule sets here, such as:
-  // js.configs.recommended,
-  ...eslintPluginAstro.configs["jsx-a11y-strict"],
+  {
+    ignores: [".github", "node_modules", "dist/**"],
+  },
+  ...pluginAstro.configs["jsx-a11y-strict"],
+  pluginPrettier,
   {
     rules: {
       // override/add rules settings here, such as:
-      // "astro/no-set-html-directive": "error"
+      // "astro/no-set-html-directive": "error",
     },
   },
 ];
