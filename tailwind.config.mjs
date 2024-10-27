1be2b5710367098fff0d4853c3089d6048e44c2c
hunghvu <hunghvu2017@gmail.com>
2024-10-27 13:58:03 -0700

    feat: make text color consistent to theme of choice across light and dark mode, #177

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  // https://tailwindcss.com/docs/dark-mode#customizing-the-selector
+  // Customize selector so it is not based on system preference
+  darkMode: ["selector", '[data-theme="dark"]'],
   theme: {
     extend: {},
   },
