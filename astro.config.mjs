20e58116bc59f237f8c0ea38f8074caaf7055833
hunghvu <hunghvu2017@gmail.com>
2024-10-28 20:10:39 -0700

    feat: add copy button to code block, #177

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,29 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
+
+import { transformerCopyButton } from "@rehype-pretty/transformers";
+import { rehypePrettyCode } from "rehype-pretty-code";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://hungvu.tech",
   integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    syntaxHighlight: false,
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          transformers: [
+            transformerCopyButton({
+              visibility: "always",
+              feedbackDuration: 2_500,
+            }),
+          ],
+        },
+      ],
+    ],
+  },
 });
