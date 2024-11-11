0de6d2b7d15dc3b7a22556da118fb7a6f213c9c6
hunghvu <hunghvu2017@gmail.com>
2024-11-10 22:20:40 -0800

    refactor: extract common variable

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const BASE_SITE_NAME = "Hung Vu"
-export const BASE_SITE_DESCRIPTION = "Navigating software, servers, and the stories in between."
-export const X_HANDLE = "@hunghvu_dev"
+export const BASE_SITE_NAME = "Hung Vu";
+export const BASE_SITE_DESCRIPTION =
+  "Navigating software, servers, and the stories in between.";
+export const X_HANDLE = "@hunghvu_dev";
+export const TRAILING_SLASH_REGEX = /\/$/;
+export const SITE_OWNER_NAME = "Huu Hung Vu";
+export const LINKEDIN_URL = "https://www.linkedin.com/in/hunghvu";
+export const GITHUB_URL = "https://github.com/hunghvu/hungvu.tech";
