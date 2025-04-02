e1ce64698b3022ab85282c134b1dad56c1e8fd61
hunghvu <hunghvu2017@gmail.com>
2025-04-01 23:36:04 -0700

    feat: migrate to daisyui v5, tailwind v4, and update to the latest astro version

--- astro.config.ts
+++ astro.config.ts
@@ -2,15 +2,14 @@ import { defineConfig } from "astro/config";
 import embeds from "astro-embed/integration";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-
 import { transformerCopyButton } from "@rehype-pretty/transformers";
 import { rehypePrettyCode } from "rehype-pretty-code";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://hungvu.tech",
-  integrations: [embeds(), mdx(), sitemap(), tailwind()],
+  integrations: [embeds(), mdx(), sitemap()],
   markdown: {
     syntaxHighlight: false,
     rehypePlugins: [
@@ -26,5 +25,8 @@ export default defineConfig({
         },
       ],
     ],
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
